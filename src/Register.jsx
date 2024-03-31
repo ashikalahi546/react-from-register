2f9cb104b5fcfada58320d86adfff4200d147d83
ashikalahi546 <144088399+ashikalahi546@users.noreply.github.com>
2024-03-31 16:05:27 +0600

    updates

--- src/Register.jsx
+++ src/Register.jsx
@@ -1,34 +1,42 @@
 import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
 import app from "./firebase.confiq";
 import { useState } from "react";
+import { IoMdEyeOff } from "react-icons/io";
+import { IoEyeOutline } from "react-icons/io5";
+
 const Register = () => {
-    const [registarError,setRegisterError] = useState('');
-    const [success,setSuccess] = useState('');
+  const [registarError, setRegisterError] = useState("");
+  const [success, setSuccess] = useState("");
+  const [showPassword, setPassword] = useState(false);
   const auth = getAuth(app);
   const handleRegister = (e) => {
     e.preventDefault();
     const email = e.target.email.value;
     const password = e.target.password.value;
     console.log(email, password);
-    if(password.length < 6){
-       setRegisterError(' Password should be at least 6 characters') ;
-       return;
+    if (password.length < 6) {
+      setRegisterError(" Password should be at least 6 characters");
+      return;
+    } else if (!/[A-Z]/.test(password)) {
+      setRegisterError(
+        "you have should have at least one uppercase characters"
+      );
+      return;
     }
-    setRegisterError('')
-    setSuccess('')
+    setRegisterError("");
+    setSuccess("");
     createUserWithEmailAndPassword(auth, email, password)
       .then((result) => {
         console.log(result.user);
-        setSuccess('user create a successfully')
+        setSuccess("user create a successfully");
       })
       .catch((error) => {
         console.error(error);
-        setRegisterError(error.message)
+        setRegisterError(error.message);
       });
   };
   return (
     <div>
-      <h3>Please Register</h3>
       <form
         onSubmit={handleRegister}
         className="flex flex-col gap-3 mt-3 w-56 mx-auto"
@@ -40,14 +48,26 @@ const Register = () => {
           placeholder="Email Address"
           required
         />
-        <input
-          className="px-3 py-2 rounded-lg"
-          type="password"
-          name="password"
-          placeholder="password"
-          id=""
-          required
-        />
+        <div className="flex relative">
+          <input
+            className="px-3 py-2 rounded-lg w-full"
+            type={showPassword ? "text" : "password"}
+            name="password"
+            placeholder="password"
+            id=""
+            required
+          />
+          <span
+            className="absolute top-3 right-2"
+            onClick={() => setPassword(!showPassword)}
+          >
+            {showPassword ? (
+              <IoEyeOutline></IoEyeOutline>
+            ) : (
+              <IoMdEyeOff></IoMdEyeOff>
+            )}
+          </span>
+        </div>
         <input
           className="px-3 py-2 rounded-lg bg-orange-500 text-white"
           type="submit"
@@ -55,14 +75,10 @@ const Register = () => {
         />
       </form>
       <div className="text-center text-red-500">
-        {
-            registarError && <p>{registarError}</p>
-        }
+        {registarError && <p>{registarError}</p>}
       </div>
       <div className="text-center text-green-500">
-{
-    success && <p>{success}</p>
-}
+        {success && <p>{success}</p>}
       </div>
     </div>
   );
